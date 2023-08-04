f1dcb8da91db3a704a74079a929443ba7e9fbc8a
ahmedhany1284 <ahmedhany4900@gmail.com>
2023-08-04 15:13:12 +0300

    update task

--- Task_2/script.js
+++ Task_2/script.js
@@ -1,23 +1,7 @@
 const box1 = document.getElementById("box1");
-const box2 = document.getElementById("box2");
-const box3 = document.getElementById("box3");
 
-function rgbToColorName(rgb) {
-    return colors[rgb] || "unknown";
-}
-// Method 1: Using addEventListener for each box
+
 box1.addEventListener("click", () => {
     console.log("Box 1 color: Red");
-    alert(" Box 1 color: Red");
 });
 
-box2.addEventListener("click", () => {
-    alert("Box 2 color: Green " );
-    console.log("Box 2 color: Green");
-
-});
-
-box3.addEventListener("click", () => {
-    console.log("Box 3 color: Blue");
-    alert("Box 3 color: Blue" );
-});

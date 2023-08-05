270c1f234a2de91b616c98c0b3c2b560e425887a
ahmedhany1284 <ahmedhany4900@gmail.com>
2023-08-05 15:43:31 +0300

    to do list task

---
+++ Task_3/script.js
@@ -0,0 +1,33 @@
+const addButton = document.getElementById('addButton');
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+
+addButton.addEventListener('click', addTask);
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== '') {
+        task = document.createElement('div');
+        task.classList.add( "task");
+        task.innerHTML = `
+            <span>${taskText}</span>
+            <div class="task-buttons">
+                <button class="done-button">Done</button>
+                <button class="delete-button">Delete</button>
+            </div>
+        `;
+        taskList.appendChild(task);
+        taskInput.value = '';
+        taskInput.focus();
+
+        const deleteButton = task.querySelector('.delete-button');
+        deleteButton.addEventListener('click', () => {
+            taskList.removeChild(task);
+        });
+
+        const doneButton = task.querySelector('.done-button');
+        doneButton.addEventListener('click', () => {
+            task.classList.toggle('completed');
+        });
+    }
+}
